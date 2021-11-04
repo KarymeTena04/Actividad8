0971f2d63f0f501139fbbe6c051a925c9a3d0bbb
KarymeTena04 <91295302+KarymeTena04@users.noreply.github.com>
2021-11-04 01:11:12 -0600

    Add files via upload

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Inicio</title>
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+	<header>
+		<div class="wrapp">
+			<div class="logo">
+            <a href="#"><img src="img/Telcel.png" alt="Telcel Logo"></a>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="index.php">Inicio</a></li>
+					<li><a href="Clientes.php">Clientes</a></li>
+					<li><a href="Empleados.php">Empleados</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
